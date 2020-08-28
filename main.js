8418257694e17223ed1d80b124cfc5a13c3780e0
Jacqueline <maaaander@gmail.com>
2020-08-28 18:01:49 -0400

    added modals

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
   })
+
+  // create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('img-thumbnail');
+// the image in the modal
+var modalImg = document.getElementById("modalImg");
+console.log(modalImg)
+
+
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(e) {
+    console.log("i get here");
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+window.onclick = function() {
+    if (event.target == modal) {
+  modal.style.display = "none";
+    }
+}
