c5b43799a7f37702325731b0be3e4b40c67a7158
Jacqueline <maaaander@gmail.com>
2020-08-31 14:00:53 -0400

    added popup

--- main.js
+++ main.js
@@ -30,3 +30,8 @@ window.onclick = function() {
   modal.style.display = "none";
     }
 }
+
+$('#headerVideoLink').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });       
