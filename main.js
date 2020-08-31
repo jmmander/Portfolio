d6d87208c5b1914b0b564602655f1c032b35e125
Jacqueline <maaaander@gmail.com>
2020-08-31 19:28:50 -0400

    fixed video bug

--- main.js
+++ main.js
@@ -1,37 +1,64 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
-  })
+
+ 
 
   // create references to the modal...
 var modal = document.getElementById('myModal');
 // to all images -- note I'm using a class!
-var images = document.getElementsByClassName('img-thumbnail');
-// the image in the modal
-var modalImg = document.getElementById("modalImg");
-console.log(modalImg)
+
+$(".img-thumbnail").on("click", function(e) {
+    imgsrc = $(this).attr("id")
+    if (imgsrc.slice(-3) === "png") {
+        $('#myModal').empty().append('<img class="modal-content" id="modalImg" />')
+        $("#modalImg").attr("src", imgsrc)
+        modal.style.display = "block";
+    }
+    else {
+        ($('#myModal').empty().append('<video class="modal-content" id="video" controls autoplay></video>'));
+        ($("#video").append('<source id="vidSource" src="" type="video/mp4" ></source>'))
+        $("#vidSource").attr("src", imgsrc)
+        modal.style.display = "block";
+    }
+})
 
 
 
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-  var img = images[i];
-  // and attach our click listener for this image.
-  img.onclick = function(e) {
-    console.log("i get here");
-    modal.style.display = "block";
-    modalImg.src = this.src;
-  }
-}
+  
+  //else {
+        //console.log("here");
+        //console.log(img.id);
+        //img.onclick = function(e) {
+          //  console.log(img.id);
+         //   console.log(2);        
+         //   var video = $('<video />', {
+          //      id: 'video',
+           //     class: "modal-content",
+           //     controls: true
+          //  });
+          //  ($('#myModal').empty().append(video));
 
-var span = document.getElementsByClassName("close")[0];
+          //  if (img.id === 'shape'){
+          //      console.log("4");
+          //      $("#video").empty();
+         //       $("#video").append('<source src="shapePlayer.mp4" type="video/mp4"></source>' );
+         //   }
+         //   if (img.id === 'turtle'){
+         //       console.log("5");
+          //      $("#video").empty().append('<source src="pythonTurtle.mp4" type="video/mp4"></source>' );
+         //   }
+         //   $("#video")[0].autoplay = true;
+         //   modal.style.display = "block";
+  //}
+//}
+//}
+
 
 window.onclick = function() {
     if (event.target == modal) {
   modal.style.display = "none";
+
     }
 }
 
-$('#headerVideoLink').magnificPopup({
-    type:'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });       
+})
