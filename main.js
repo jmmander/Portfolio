14fe888492a6497d996d5174bef83a21ef1025e4
Jacqueline <maaaander@gmail.com>
2020-09-02 17:45:21 -0400

    added mixitup library

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+
+
 $(document).ready(function(){
     $('.header').height($(window).height());
 
- 
 
-  // create references to the modal...
+
+//selects correct modal image based on user click event
 var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
 
 $(".img-thumbnail").on("click", function(e) {
-    imgsrc = $(this).attr("id")
+    const imgsrc = $(this).attr("id")
     if (imgsrc.slice(-3) === "png") {
         const caption =  $(this).attr("alt");
         $('#myModal').empty().append('<img class="modal-content " id="modalImg" /> <figcaption class="figure-caption"><figcaption>');
@@ -26,36 +27,33 @@ $(".img-thumbnail").on("click", function(e) {
     }
 })
 
+var containerEl = document.querySelector('.row');
+
+var mixer = mixitup(containerEl);
+
+//selects correct projets based on tags
+
+
+    //if (images[i].attr("display") == "none") {
+      //(images[i].attr("display")) = "none !important";
+    //}
+
+ // const selection = $(this).attr("id");
+  //console.log(selection);
+  //if (selection === "python"){
+  //  console.log("here")
+  //  const pics = document.getElementsByClassName("js");
+   // for (let i = 0; i < pics.length; i++){
+    //  pics[i].style.display = "hidden";
+    //    }
+   //  }
+
+
+//})
 
 
   
-  //else {
-        //console.log("here");
-        //console.log(img.id);
-        //img.onclick = function(e) {
-          //  console.log(img.id);
-         //   console.log(2);        
-         //   var video = $('<video />', {
-          //      id: 'video',
-           //     class: "modal-content",
-           //     controls: true
-          //  });
-          //  ($('#myModal').empty().append(video));
-
-          //  if (img.id === 'shape'){
-          //      console.log("4");
-          //      $("#video").empty();
-         //       $("#video").append('<source src="shapePlayer.mp4" type="video/mp4"></source>' );
-         //   }
-         //   if (img.id === 'turtle'){
-         //       console.log("5");
-          //      $("#video").empty().append('<source src="pythonTurtle.mp4" type="video/mp4"></source>' );
-         //   }
-         //   $("#video")[0].autoplay = true;
-         //   modal.style.display = "block";
-  //}
-//}
-//}
+  
 
 
 window.onclick = function() {
